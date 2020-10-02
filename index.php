cf42a4c56e8310ecd819ea042466adcc5763e614
laurencekeithabaring <72242203+laurencekeithabaring@users.noreply.github.com>
2020-10-02 20:42:57 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>LOIS' LECHON</title>
+  <link href="style.css" rel="stylesheet">
+  <link rel="stylesheet"
+</head>
+<body>
+	<div class="box-area">
+	<header>
+		<div class='diamond'>
+				LOIS' LECHON
+		</div>
+		
+					<div class="leaf1"></div>
+					<div class="leaf1"></div>
+					<div class="leaf1"></div>
+					
+				
+	</div>
+	</header>
+		<div class="banner-area">
+		<div class="text"> <h2>LOIS' LECHON</h2>
+		</div>
+		
+
+		<div class="box">
+		<ul> Opening Hours</u>
+				<ul>Mon:	7:00 am – 8:00 pm</ul>
+				<ul>Tue:	7:00 am – 8:00 pm</ul>
+				<ul>Wed:	7:00 am – 8:00 pm</ul>
+				<ul>Thu:	7:00 am – 8:00 pm</ul>
+				<ul>Fri:	7:00 am – 8:00 pm</ul>
+				<ul>Sat:	7:00 am – 8:00 pm</ul>
+				<ul>Sun:	7:00 am – 8:00 pm</ul>
+		</div>
+		
+					<div class="box1">
+								<ul>FOR PICK UP AND TAKE-OUT ONLY</ul> <br>
+						<ul>Lechon per kilo</ul>
+						<ul>PHP 650.00</ul>
+						<ul>for pick-up and take out only</ul>
+						<ul>Whole Lechon</ul>
+						<ul>PHP 5,500.00</ul>
+						<ul>for orders 5days reservation</ul>
+	</div>
+	<div class='diamond1'>
+					  
+					</div>
+					<div class='lois'>
+					  <h2>LOIS' LECHON</h2>
+					</div>
+	</div>
+		<div class="content-area">
+		<div class="wrapper">
+			
+			
+				<h3><ul>We are a family-run business specialising in the most best-selling Lechon in Bacolod. </ul> 
+					
+			
+			
+				<p>Restaurant in Bacolod</p>
+				<p>Open today until 20:00</p></h3> <br>
+				<h2>Address</h2>
+			</div>
+			<div class="map-responsive">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12191.476334280296!2d122.94250296385484!3d10.666668915367826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33aed034f6524469%3A0x26acff3704dca696!2sLECHONAN%20COUNTRY!5e1!3m2!1sen!2sph!4v1599287623980!5m2!1sen!2sph" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+								width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+							</div>
+						
+			 <div class="head">
+				<div class="eye">
+				  <div class="eyeball"></div>
+				</div>
+				
+				<div class="eye">
+				  <div class="eyeball"></div>
+				</div>
+				
+				<div class="nose">
+				  <div class="nostril"></div>
+				  <div class="nostril"></div>
+				</div>
+			  </div>
+
+									
+							<div class="column">
+						<label for="fname">Full name</label>
+							<input type="text" id="fname" name="firstname" placeholder="Full Name...">
+								<label for="lname">Email Address</label>
+									<input type="text" id="ename" name="emailaddress" placeholder="E-mail Address...">
+									<div class="column1">
+								<label for="mes">Message</label>
+							<input type="text1" id="" name="mes" placeholder="send us a message...">
+						<input type="submit" value="Submit">
+						
+					</div>
+					</div>
+					<div class="welcome"	
+					<h1>WELCOME!</h1></div>
+					<div class="leaf"></div>
+					<div class="leaf"></div>
+					<div class="leaf"></div>
+					
+					<div class="wrap">
+								<div class="search">
+							  <input type="text" class="searchTerm" placeholder="What are you looking for?">
+							  <button type="submit" class="searchButton">
+								Search
+							 </button>
+						  </div>
+						</div>
+					
+					
+					
+				<div class=  "footer">
+				<div class = "facebook"></div>
+				<div class = "twitter"></div>
+				<div class="instagram">
+				<div class="shape"></div>
+				</div>
+				<p>Copyright © 2019 All rights reserved. </p>
+			</div>
+			
+		</div>
+		
+	
+		
+	</body>
+</html>	
